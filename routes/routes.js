31f998b577957d91ea0b21b0f42c9ee4cda94d2d
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-15 11:07:28 -0600

    Add post request and update get to have a try/catch

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,24 @@ const db = require('../db/db')
 
 const router = express.Router()
 
-router.get('/project', async (req, res) => {
-  const tickets = await db('tickets').select()
-  res.status(200).json(tickets)
+router.get('/tickets', async (req, res) => {
+  try {
+    const tickets = await db('tickets').select()
+    res.status(200).json(tickets)
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating ticket', error: err })
+  }
+})
+
+router.post('/add-ticket', async (req, res) => {
+  try {
+    const { title, description, status } = req.body
+    const newTicket = { title, description, status }
+    const createdTicket = await db('tickets').insert(newTicket).returning(['id', 'title', 'description', 'status'])
+    res.status(201).json(createdTicket)
+  } catch (err) {
+    res.status(500).json({ message: 'Error creating ticket', error: err })
+  }
 })
 
 module.exports = router
