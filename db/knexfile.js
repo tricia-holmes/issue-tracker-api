983f610f78ef0fc3b7790a67e63ee3b7a53f26f5
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-15 10:17:58 -0600

    Add db and config files

---
+++ db/knexfile.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const knex = require('knex')
+
+const knexfile = require('./knexfile')
+
+const env = process.env.NODE_ENV
+const configOptions = knexfile[env]
+
+module.exports = knex(configOptions)
