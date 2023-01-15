d7ce344029f0f40e667d558986e2c09c23411442
Tricia Holmes <tricia.michele.holmes@gmail.com>
2023-01-15 12:00:12 -0600

    Update put endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/tickets', async (req, res) => {
   try {
-    const tickets = await db('tickets').select()
+    const tickets = await db('tickets').select().orderBy('id')
     res.status(200).json(tickets)
   } catch (err) {
     res.status(500).json({ message: 'Error getting tickets', error: err })
@@ -23,14 +23,19 @@ router.post('/tickets', async (req, res) => {
   }
 })
 
-router.patch('/tickets/:id', async (req, res) => {
+router.put('/tickets/:id', async (req, res) => {
   try {
-    console.log('hello?')
     const { title, description, status } = req.body
     const ticket = await db('tickets')
       .where({ id: req.params.id })
       .update({ title, description, status })
       .returning(['id', 'title', 'description', 'status'])
+
+    if (!ticket.length) {
+      res.status(400)
+      return res.json({ message: 'Ticket not found' })
+    }
+
     res.status(200).json(ticket)
   } catch (err) {
     res.status(500).json({ message: 'Error updating ticket', error: err })
